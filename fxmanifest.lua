adc5ee84d8d855d10fb8a3f05f38429dc6d2e811
deivuks420yxz <83653223+deivuks420yxz@users.noreply.github.com>
2021-05-04 17:25:52 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'adamant'
+game 'gta5'
+
+files {
+    'index.html',
+    'css/index.css',
+    'img/cursor.png',
+    'music/music.mp3',
+    'img/back.png',
+    'js/index.js',
+    'js/main.js',
+    'js/plugins.js',
+    'font/HopsAndBarleyc3-Bold.otf',
+    'font/HopsAndBarley1-Bold.otf',
+    'font/HopsAndBarleyc3-Regular.otf',
+}
+
+loadscreen 'index.html'
---
+++ js/index.js
@@ -0,0 +1,37 @@
+
+var audio = document.getElementById("sound");
+audio.volume = 0.1;
+
+var isPlaying = false;
+
+function paleistiMuzika() {
+    isPlaying ? sound.pause() : sound.play();
+  };
+  
+  sound.onplaying = function() {
+    isPlaying = true;
+    document.getElementById("play").className = "fas fa-pause-circle  play";
+  };
+  sound.onpause = function() {
+    isPlaying = false;
+    document.getElementById("play").className = "fas fa-play-circle  play";
+  };
+
+var audio = document.getElementById("sound");
+
+function vlSumazinti() { 
+  audio.volume -= 0.1;
+} 
+
+function vlPadidinti() { 
+    audio.volume += 0.1;
+  }
+
+var pointer = $('#cursor');
+$(document).on('mousemove', function (event) {
+  pointer.css({
+    top: event.pageY + 'px',
+    left: event.pageX + 'px'
+  });
+});
+
